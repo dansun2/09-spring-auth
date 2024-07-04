c530f1e047aad1e9afea30258807b46780856405
dansun2 <dansunmusik7@gmail.com>
2024-07-04 12:39:24 +0900

    240704

--- chap01-security-session/src/main/java/com/ohgiraffers/security/user/controller/UserController.java
+++ chap01-security-session/src/main/java/com/ohgiraffers/security/user/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.ohgiraffers.security.user.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/signup")
+    public void signup(){
+
+    }
+
+    @PostMapping("/signup")
+    public ModelAndView signup(@ModelAttribute SignupDTO signupDTO, ModelAndView mv){
+
+    }
 }
---
+++ chap01-security-session/src/main/java/com/ohgiraffers/security/user/dto/SignupDTO.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.security.user.dto;
+
+public class SignupDTO {
+}
