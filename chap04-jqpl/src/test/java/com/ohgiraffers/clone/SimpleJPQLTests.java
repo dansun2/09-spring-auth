50b7bea609b5e04d49fc6599a82dfc498df921cb
dansun2 <dansunmusik7@gmail.com>
2024-07-08 10:39:49 +0900

    240708

--- chap04-jqpl/src/test/java/com/ohgiraffers/clone/SimpleJPQLTests.java
+++ chap04-jqpl/src/test/java/com/ohgiraffers/clone/SimpleJPQLTests.java
@@ -1,4 +1,23 @@
 package com.ohgiraffers.clone;
 
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 public class SimpleJPQLTests {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("");
+    }
+    @BeforeEach
+    public void initManager(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
 }
