75f05239e6a9f38be8261b1b4980ce11f79a2dd4
dansun2 <dansunmusik7@gmail.com>
2024-07-09 09:53:59 +0900

    240709

--- chap04-jqpl/src/test/java/com/ohgiraffers/section03/projection/ProjectionTests.java
+++ chap04-jqpl/src/test/java/com/ohgiraffers/section03/projection/ProjectionTests.java
@@ -42,6 +42,18 @@ public class ProjectionTests {
     * 1. 엔티티 프로젝션
     *   원하는 객체를 바로 조회할 수 있다.
     *   조회된 엔티티는 영속성 컨텍스트에서 관리한다.
+    *
+    * 2. 임베디드 타입 프로젝션
+    *   엔티티와 거의 비슷하게 사용되며 조회의 시작점이 될 수 없다. from절에 사용 불가
+    *   임베디드 타입은 영속성 컨텍스트에서 관리되지 않는다.
+    *
+    * 3. 스칼라 타입 프로젝션
+    *   숫자, 문자, 날짜 같은 기본 데이터 타입이다.
+    *   스칼라 타입은 영속성 컨텍스트에서 관리되지 읂는다.
+    *
+    * 4. new 명령어를 활용한 프로젝션
+    *   다양한 종류의 단순 값들을 DTO로 바로 조회하는 방식으로 new 패키지명.DTO명을 쓰면 해당 DTO로 바로 반환받을 수 있다.
+    *   new명령어를 사용한 클래스의 객체는 엔티티가 아니므로 영속성 컨텍스트에서 관리되지 않는다.
     * */
 
 
@@ -77,6 +89,17 @@ public class ProjectionTests {
         //then
         Assertions.assertNotNull(menuInfoList);
         menuInfoList.forEach(System.out::println);
-
     }
+
+    // 3. 스칼라 타입 프로젝션
+    @Test
+    public void TypedQuery를_이용한_스칼라_타입_프로젝션_테스트(){
+        String jpql = "SELECT m.categoryName FROM category_section03";
+
+        List<String> categoryNameList = entityManager.createQuery(jpql, String.class).getResultList();
+
+        Assertions.assertNotNull(categoryNameList);
+        categoryNameList.forEach(System.out::println);
+    }
+
 }
