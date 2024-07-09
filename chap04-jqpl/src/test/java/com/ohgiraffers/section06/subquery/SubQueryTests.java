49e72883a2b0079d53ec3c2f7741058a1019b2b2
dansun2 <dansunmusik7@gmail.com>
2024-07-09 11:36:10 +0900

    240709

---
+++ chap04-jqpl/src/test/java/com/ohgiraffers/section06/subquery/SubQueryTests.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section06.subquery;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SubQueryTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory(){ entityManagerFactory = Persistence.createEntityManagerFactory("jpatest"); }
+    @BeforeEach
+    public void initManager(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+    @AfterEach
+    public void closeManager(){
+        entityManager.close();
+    }
+    @AfterAll
+    public static void closeFactory(){
+        entityManagerFactory.close();
+    }
+
+}
