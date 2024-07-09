4951d5a15b7925ba9a6d362fd4baa3dd8d62b6f5
dansun2 <dansunmusik7@gmail.com>
2024-07-09 10:07:00 +0900

    240709

--- chap04-jqpl/src/test/java/com/ohgiraffers/section04/groupfunction/GroupFunctionTests.java
+++ chap04-jqpl/src/test/java/com/ohgiraffers/section04/groupfunction/GroupFunctionTests.java
@@ -30,4 +30,13 @@ public class GroupFunctionTests {
         entityManagerFactory.close();
     }
 
+    /*
+    * JPQL의 그룹함수는 COUNT, MAX, MIN, SUM, AVG로 SQL의 그룹함수와 별반 차이가 없다.
+    * 단, 몇가지 주의사항이 있다.
+    * 1. 그룹함수의 반환 타입은 결과 값이 정수면 Long, 실수면 Double로 반환된다.
+    * 2. 값이 없는 상태에서는 count를 제외한 그룹 함수는 null이 되고, count만 0이 된다.
+    *   따라서 반환 값을 담기 위해 선언하는 변수 타입을 기본자료형으로 하게 되면, 조회 결과를 언방식 할때 npe가 발생한다.
+    * 3. 그룹 함수의 반환 자료형은 Long or Double형이기 떄문에 having절에서 그룹 함수 결과값을 비교하기 위한
+    *   파라미터 타입은 Long or Double로 해야한다.
+    * */
 }
