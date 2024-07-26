e2133f57f93f1d475dbc9250c64f8578e4da0f95
dansun2 <dansunmusik7@gmail.com>
2024-07-26 10:09:40 +0900

    240726

--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/model/entity/OhUser.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/model/entity/OhUser.java
@@ -2,6 +2,10 @@ package com.ohgiraffers.chap02securityjwt.user.model.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Entity
 @Table(name = "TBL_USER")
 public class OhUser {
@@ -29,4 +33,80 @@ public class OhUser {
 
     @Column(name = "USER_STATE")
     private String state;
+
+    public List<String> getRoleList(){
+        if(this.role.getRole().length() > 0) {
+            return Arrays.asList(this.role.getRole().split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public int getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(int userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public OhgiraffersRole getRole() {
+        return role;
+    }
+
+    public void setRole(OhgiraffersRole role) {
+        this.role = role;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "OhUser{" +
+                "userNo=" + userNo +
+                ", userId='" + userId + '\'' +
+                ", userPass='" + userPass + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", role=" + role +
+                ", state='" + state + '\'' +
+                '}';
+    }
 }
--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/model/entity/OhgiraffersRole.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/model/entity/OhgiraffersRole.java
@@ -3,11 +3,15 @@ package com.ohgiraffers.chap02securityjwt.user.model.entity;
 public enum OhgiraffersRole {
     USER("USER"),
     ADMIN("ADMIN"),
-    ALL("USER,ADMIN");
+    ALL("USER,ADMIN"); // 다중권한방식
 
     private String role;
 
     OhgiraffersRole(String role) {
         this.role = role;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
