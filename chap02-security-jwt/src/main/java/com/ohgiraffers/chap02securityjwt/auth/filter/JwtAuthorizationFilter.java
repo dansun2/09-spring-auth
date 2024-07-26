0ea00bfd2be850a99180700ff82b5c553c3d5636
dansun2 <dansunmusik7@gmail.com>
2024-07-26 16:10:07 +0900

    240726

--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/auth/filter/JwtAuthorizationFilter.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/auth/filter/JwtAuthorizationFilter.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.chap02securityjwt.auth.filter;
 
+import com.ohgiraffers.chap02securityjwt.common.AuthConstants;
+import com.ohgiraffers.chap02securityjwt.common.utils.TokenUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +10,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 // 요청시 기본필터
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
@@ -18,6 +22,23 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        super.doFilterInternal(request, response, chain);
+        // 권한이 필요없는 요청목록들 설정
+        List<String> rolelessList = Arrays.asList("/signup");
+
+        // '/signup' 이걸 포함하고 있어? -> true면 끝내버리고 다음 메서드
+        if(rolelessList.contains(request.getRequestURI())){ // 요청받은 uri가 rolelesslist에 포함되어있냐
+            chain.doFilter(request,response);
+            return;
+        }
+
+        // 베어
+        String header = request.getHeader(AuthConstants.AUTH_HEADER);
+
+        // 사용자가 요청을 할 시 헤더정보에 뭔가 있다고 하면 밸류를 꺼내옴
+        try {
+            if (header != null && !header.equalsIgnoreCase("")) {
+                String token = TokenUtils.splitHeader(header);
+            }
+        }
     }
 }
