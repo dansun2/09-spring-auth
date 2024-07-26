255d7ad710a245ebe8d2cea7c3977e7718f72939
dansun2 <dansunmusik7@gmail.com>
2024-07-26 10:32:37 +0900

    240726

--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/controller/UserController.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/controller/UserController.java
@@ -1,7 +1,20 @@
 package com.ohgiraffers.chap02securityjwt.user.controller;
 
+import com.ohgiraffers.chap02securityjwt.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private UserService userService;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserController(UserService userService, BCryptPasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
 }
--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/service/UserService.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/service/UserService.java
@@ -3,11 +3,17 @@ package com.ohgiraffers.chap02securityjwt.user.service;
 import com.ohgiraffers.chap02securityjwt.user.model.UserDTO;
 import com.ohgiraffers.chap02securityjwt.user.model.entity.OhUser;
 import com.ohgiraffers.chap02securityjwt.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public UserDTO saveUser(UserDTO saveUserDTO){
         // 비즈니스 로직 추가
@@ -23,5 +29,7 @@ public class UserService {
         saveUserDTO.setUserId(saveUser.getUserId());
         saveUserDTO.setUserPass(saveUser.getUserPass());
         saveUserDTO.setUserName(saveUser.getUserName());
+
+        return saveUserDTO;
     }
 }
