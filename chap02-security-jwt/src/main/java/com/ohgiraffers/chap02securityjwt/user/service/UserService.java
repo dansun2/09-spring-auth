2b20e2fe05a5169e0e528604d897b0fe0b27d82d
dansun2 <dansunmusik7@gmail.com>
2024-07-26 10:21:26 +0900

    240726

--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/service/UserService.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.chap02securityjwt.user.service;
 
+import com.ohgiraffers.chap02securityjwt.user.model.UserDTO;
 import com.ohgiraffers.chap02securityjwt.user.model.entity.OhUser;
 import com.ohgiraffers.chap02securityjwt.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private UserRepository userRepository;
 
-    public OhUser saveUser(OhUser ohUser){
+    public UserDTO saveUser(UserDTO saveUserDTO){
+        // 비즈니스 로직 추가
 
+        OhUser ohUser = new OhUser();
+        ohUser.setUserName(saveUserDTO.getUserName());
+        ohUser.setUserId(saveUserDTO.getUserId());
+        ohUser.setUserPass(saveUserDTO.getUserPass());
+
+        OhUser saveUser = userRepository.save(ohUser);
+
+        // 등록 완료 로직 추가
+        saveUserDTO.setUserId(saveUser.getUserId());
+        saveUserDTO.setUserPass(saveUser.getUserPass());
+        saveUserDTO.setUserName(saveUser.getUserName());
     }
 }
