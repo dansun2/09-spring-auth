edda48ff154d587990beee5b1e5852510c3e3f5c
dansun2 <dansunmusik7@gmail.com>
2024-07-26 15:47:37 +0900

    240726

--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/auth/handler/CustomAuthSuccessHandler.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/auth/handler/CustomAuthSuccessHandler.java
@@ -1,7 +1,9 @@
 package com.ohgiraffers.chap02securityjwt.auth.handler;
 
 import com.ohgiraffers.chap02securityjwt.auth.model.DetailsUser;
+import com.ohgiraffers.chap02securityjwt.common.AuthConstants;
 import com.ohgiraffers.chap02securityjwt.common.utils.ConvertUtil;
+import com.ohgiraffers.chap02securityjwt.common.utils.TokenUtils;
 import com.ohgiraffers.chap02securityjwt.user.model.entity.OhUser;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class CustomAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
@@ -23,8 +26,23 @@ public class CustomAuthSuccessHandler extends SavedRequestAwareAuthenticationSuc
 
         JSONObject jsonObject;
 
-        if (user.getState().equals("N")) {
+        if (user.getState().equals("N")) { // 유저 정보가 N이면 탈퇴회원이거나 비활성화회원
+            responseMap.put("userInfo", jsonValue);
+            responseMap.put("message", "탈퇴한 회원입니다.");
+        }else {
+            String token = TokenUtils.generateJwtToken(user);
+            responseMap.put("userInfo", jsonValue);
+            responseMap.put("message", "로그인 성공");
 
+            response.addHeader(AuthConstants.AUTH_HEADER, AuthConstants.TOKEN_TYPE + " " + token);
         }
+
+        jsonObject = new JSONObject(responseMap);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonObject);
+        printWriter.flush();
+        printWriter.close();
     }
 }
