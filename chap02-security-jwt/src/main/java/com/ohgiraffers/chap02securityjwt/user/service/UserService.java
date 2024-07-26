ffdad94545fe36f5a05f359fc412e610fff682be
dansun2 <dansunmusik7@gmail.com>
2024-07-26 10:17:36 +0900

    240726

--- chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/service/UserService.java
+++ chap02-security-jwt/src/main/java/com/ohgiraffers/chap02securityjwt/user/service/UserService.java
@@ -1,7 +1,14 @@
 package com.ohgiraffers.chap02securityjwt.user.service;
 
+import com.ohgiraffers.chap02securityjwt.user.model.entity.OhUser;
+import com.ohgiraffers.chap02securityjwt.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+
+    public OhUser saveUser(OhUser ohUser){
+
+    }
 }
